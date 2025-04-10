b1c567df308051a6c39758108f5ea26eabdb3273
Nayem Uddin <nayemuddin2006@gmail.com>
2025-04-10 23:05:49 +0600

    Update UserInformation.java

--- Java Class/UserInformation.java
+++ Java Class/UserInformation.java
@@ -3,15 +3,14 @@ package com.mycompany.banking_management_system;
 
 public class UserInformation {
     
-    private String userId,userName,password,email;
+    private String userId,userName,passwordl;
     
     //Constructor
-     public UserInformation(String userId, String userName, String password, String email){
+     public UserInformation(String userId, String userName, String password){
         
         this.userId = userId;
         this.userName = userName;
         this.password = password;
-        this.email =  email;
     }
 
      //Setter getter method to access Private variable.
@@ -35,19 +34,11 @@ public class UserInformation {
      public String getPassword(){
          return password;
      }
-     
-     public void setEmail(String email){
-         this.email = email;
-     }
-     public String getEmail(){
-         return email;
-     }
     
      //Method
     public void displayInformation(){
         System.out.println("User ID : "+userId);
         System.out.println("User Name : "+userName);
-        System.out.println("Email : "+email);
         System.out.println();
     }
 }
